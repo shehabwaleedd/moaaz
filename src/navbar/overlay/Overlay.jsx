c115a0b0483590e29df80e4d644d8aa73e4a325f
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-26 13:51:42 +0400

    improvments

--- src/navbar/overlay/Overlay.jsx
+++ src/navbar/overlay/Overlay.jsx
@@ -11,28 +11,28 @@ const Overlay = ({handleMenuClose}) => {
                 <motion.div className="overlay__list" initial={{ opacity: 0, y: -100, transition: { delay: 0.3, staggerChildren: 3.5, duration: 0.5, ease: [0.42, 0, 0.58, 1] } }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: 500, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
                     <ul>
                         <li>
-                            <Link to="/" class="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
+                            <Link to="/" className="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
                                 <span area-hidden="true">
                                     <span>01 Home<span>01 Home</span></span>
                                 </span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/about" class="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
+                            <Link to="/about" className="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
                                 <span area-hidden="true">
                                     <span>02 About<span>02 About</span></span>
                                 </span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/work" class="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
+                            <Link to="/work" className="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
                                 <span area-hidden="true">
                                     <span>03 Work<span>03 Work</span></span>
                                 </span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/contact" class="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
+                            <Link to="/contact" className="hover-link" area-label="Hover Me" cursor-type="button" onClick={handleMenuClose}>
                                 <span area-hidden="true">
                                     <span>04 Contact<span>04 Contact</span></span>
                                 </span>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     return (
         <motion.section className='contact'
             initial={{ opacity: 0, y: 150 }}
-            animate={{ opacity: 1, y: 0, type: "tween", transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }}
             exit={{ opacity: 0, y: 150, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}
         >
             <img src={img} alt="" />
