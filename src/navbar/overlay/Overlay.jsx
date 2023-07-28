8ac57e149543a55d022422a2f98699695cb91380
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-28 06:22:31 +0400

    footer in both contact and work is responsive now

--- src/navbar/overlay/Overlay.jsx
+++ src/navbar/overlay/Overlay.jsx
@@ -1,20 +1,7 @@
 import React from 'react'
-import './Overlay.css'
 import MobileResponsive from './overlayResponsive/mobileResponsive/MobileResponsive'
 import DesktopResponsive from './overlayResponsive/desktopResponsive/DesktopResponsive'
 
-const Overlay = ({ handleMenuClose, isMobile }) => {
-    return (
-        <>
-            {
-                isMobile ? (
-                    <MobileResponsive handleMenuClose={handleMenuClose} />
-                ) : (
-                    <DesktopResponsive handleMenuClose={handleMenuClose} />
-                )
-            }
-        </>
-    )
-}
+const Overlay = ({ handleMenuClose, isMobile }) => { return ( <> { isMobile ? ( <MobileResponsive handleMenuClose={handleMenuClose} /> ) : ( <DesktopResponsive handleMenuClose={handleMenuClose} />)}</>) }
 
 export default Overlay
