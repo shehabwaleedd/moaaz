42c9c6eb82fec97e7c5edaedf5e6369d480bf619
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-03 11:18:56 +0400

    fixed responsivness issue in big monitors

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -18,6 +18,7 @@ const Work = () => {
         <motion.section className='work'>
             <motion.div className="video__background" initial={{ opacity: 0, y: -100, transition: { delay: 0.3, staggerChildren: 3.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: 500, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
                 <video src={video} controls loop autoPlay muted />
+                <h1>My Work</h1>
             </motion.div>
             <motion.div className="work__container" initial={{ opacity: 0, y: 100, transition: { delay: 0.3, staggerChildren: 3.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: 500, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
                 <div className="video-grid">
@@ -25,7 +26,6 @@ const Work = () => {
                         <div className="video-item" key={video.id}>
                             <video controls>
                                 <source src={video.url} type="video/mp4" />
-                                Your browser does not support the video tag.
                             </video>
                         </div>
                     ))}
